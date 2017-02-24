a26008ceff529ee40d322f7e38805f98dbcae784
bonepeople <bonepeople@sina.com>
2017-02-24 19:04:28 +0800

    完成主要功能的实现
    程序根据之前获取的渠道名称一次进行签名，签名所涉及到的文件由用户自行指定
    Config.java中保存的是私有的密码信息，用户可以直接将APKSigner中的值改为自己所使用的值

--- src/com/bonepeople/tools/APKSigner.java
+++ src/com/bonepeople/tools/APKSigner.java
@@ -1,6 +1,10 @@
 package com.bonepeople.tools;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
@@ -15,12 +19,20 @@ import java.nio.file.StandardOpenOption;
  */
 public class APKSigner
 {
-	private static final String CHANNELFILE = "UMENG_CHANNEL.txt";
+	private static final String CHARSET = "GBK";// 这个是Windows下的默认字符集，输出信息不正常时可以调整此项
+	private static final String CHANNELFILE = "UMENG_CHANNEL.txt";// 这个是渠道文件的文件路径
+	private static final String KEYSTOREFILE = "key";// 这个是密钥文件的文件路径
+	public static final String STOREPASS = Config.STOREPASS;// 这个是密钥文件的密码
+	public static final String KEYNAME = Config.KEYNAME;// 这个是密钥中签名的别名
 	private static String[] _channel_names;
 
 	public APKSigner()
 	{
 		get_channel_name();
+		for (String _channel_name : _channel_names)
+		{
+			sign(_channel_name);
+		}
 	}
 
 	private void get_channel_name()
@@ -56,6 +68,61 @@ public class APKSigner
 					e.printStackTrace();
 				}
 		}
+	}
+
+	private void sign(String _channel_name)
+	{
+		if (new File(get_unsignedFilePath(_channel_name)).exists())
+		{
+			String _cmd = "jarsigner -keystore " + KEYSTOREFILE + " -storepass " + STOREPASS + " -signedjar " + get_signedFilePath(_channel_name) + " " + get_unsignedFilePath(_channel_name) + " "
+					+ KEYNAME;
+			InputStream _in = null;
+			try
+			{
+				System.out.println("正在签名：" + get_signedFilePath(_channel_name));
+				Process _process = Runtime.getRuntime().exec(_cmd);
+				_in = _process.getInputStream();
+				BufferedReader _reader = new BufferedReader(new InputStreamReader(_in, Charset.forName(CHARSET)));
+				try
+				{
+					String _line = "";
+					while ((_line = _reader.readLine()) != null)
+					{
+						System.out.println(_line);
+					}
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+				_reader.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			System.out.println(get_unsignedFilePath(_channel_name) + " NotFound");
+		}
+	}
+
+	/**
+	 * 获取对应渠道未签名文件的路径，可以在这里自定义文件名
+	 */
+	private String get_unsignedFilePath(String _channel_name)
+	{
+		return "unsigned\\" + _channel_name + ".apk";
+	}
+
+	/**
+	 * 获取对应渠道已签名文件的路径，可以在这里自定义文件名
+	 */
+	private String get_signedFilePath(String _channel_name)
+	{
+		String _version = "_1.0.0";
+		return "signed\\" + _channel_name + _version + ".apk";
 	}
 
 	public static void main(String[] args)
